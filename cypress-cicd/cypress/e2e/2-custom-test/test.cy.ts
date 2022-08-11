41a2c0633937ab878fa94c0f65816c2386218f7f
dani-capa <dani@capa.ai>
2022-08-11 16:46:03 +0900

    init

---
+++ cypress-cicd/cypress/e2e/2-custom-test/test.cy.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe("example to-do app", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("displays two todo items by default", () => {
+    cy.contains("Edit src/App.tsx and save to reload.").should("exist");
+  });
+});
